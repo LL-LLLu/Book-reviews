0f46636c0abf73553795b5bcfa2ebe33b08acc7e
Qi Lu <qilu@Mac.attlocal.net>
2025-12-20 14:19:05 -0600

    gitignore update

--- backend/server.js
+++ backend/server.js
@@ -59,8 +59,8 @@ app.use('/uploads', express.static('uploads'));
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 1000 // limit each IP to 1000 requests per minute
 });
 app.use('/api/', limiter);
 
